e86b8bd6f726d75a7ee8b1dd87d2d9a0c89c6a05
Dylan Barber <dyleighba@gmail.com>
2020-10-29 16:01:08 +0800

    Created Renderer abstract class

---
+++ core/src/com/leibeir/lifesimulator/render/Renderer.java
@@ -0,0 +1,30 @@
+package com.leibeir.lifesimulator.render;
+
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.utils.Array;
+import com.leibeir.lifesimulator.api.IRenderer;
+
+public abstract class Renderer implements IRenderer {
+    protected ModelBatch modelBatch = new ModelBatch();
+    protected Array<ModelInstance> modelInstances = new Array<>();
+
+    public Renderer() {
+
+    }
+
+    public void update() {};
+
+    public void render(Camera camera, Environment environment) {
+        modelBatch.begin(camera);
+        modelBatch.render(modelInstances, environment);
+        modelBatch.end();
+    }
+
+    public void dispose() {
+        modelBatch.dispose();
+        modelInstances.clear();
+    }
+}
